427b27fcb1cc1ac18f53a704a5d6652564626bb6
serhiion <onyserhii@gmail.com>
2021-03-16 20:18:02 +0200

    16.03

--- laba7/1.py
+++ laba7/1.py
@@ -17,47 +17,62 @@ print("Другий масив:")
 print(B)
 print()
 
-print("Елементи які є в першому масиві, без повторень: ")
-list1 = [item for item in set(A) if A.count(item) < 2]
-print(list1)
-print("Елементи які повторюються в першому масиві (для перевірки): ")
-test = [item for item in set(A) if A.count(item) > 1]
-print(test)
 
-list2 = [item for item in set(B) if B.count(item) > 1]
-print("Елементи які повторюються в другому масиві: ")
-print(list2)
 
-c = []
-def lin():
-    for i in list1:
-        for j in list2:
+def binary(array, value, index):
+    left = 0
+    right = len(array) - 1
+    while left <= right:
+        middle = (left + right) // 2
+
+        if value == array[middle] and index != middle:
+            return True
+        if value < array[middle]:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return False
+
+
+def lin(array, value, index):
+    for i in range(len(array)):
+        if array[i] == value and i != index:
+            return i
+    return -1
+
+
+def common_row_method(a, index):
+    n = len(a)
+    array = []
+    for i in range(n):
+        array.append([])
+        for j in range(n):
+            array[i].append(False)
+
+    for i in range(len(array)):
+        for j in range(len(array[i])):
             if i == j:
-                c.append(i)
-    print(c)
+                continue
+            if a[i] == a[j]:
+                array[i][j] = True
 
-print("Фінальний масив:")
-lin()
+    for i in range(len(array)):
+        if array[index][i]:
+            return False
 
-print()
-final = sorted(list1 + list2)
-print(final)
+    return True
 
+# Виводить ті які є декілька в другому, і унікальні в першому
+for i in range(len(A)):
+    if not binary(A, A[i], i):
+        secondArrayHave = lin(B, A[i], -1)
+        if secondArrayHave != -1 and lin(B, A[i], secondArrayHave) != -1:
+            print("Фінальний: ", A[i])
 
-
-# mid = len(final) // 2
-# low = 0
-# high = len(final) - 1
-# for item in c:
-#     while final[mid] != item and low <= high:
-#         if item > final[mid]:
-#             low = mid + 1
-#         else:
-#             high = mid - 1
-#         mid = (low + high) // 2
-#
-#     if low > high:
-#         print("Чисел немає! ")
-#     else:
-#         print("ID = ", mid)
+# Виводить ті які є декілька в першому, і унікальні в другому
+for i in range(len(B)):
+    if common_row_method(B, i):
+        secondArrayHave = lin(A, B[i], -1)
+        if secondArrayHave != -1 and lin(A, B[i], secondArrayHave) != -1:
+            print("Фінальний b: ", B[i])
 
